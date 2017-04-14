81c33f9c8d151872dfdac13c96957105857cc0e9
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2017-04-14 02:03:18 -0400

    add

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-   devise_scope :user do
-    get '/register', to: 'devise/registrations#new', as: :register
-    get '/log-in', to: 'devise/sessions#new', as: :login
-  end
+  #  devise_scope :user do
+  #   get '/register', to: 'devise/registrations#new', as: :register
+  #   get '/log-in', to: 'devise/sessions#new', as: :login
+  # end
 
   # You can have the root of your site routed with "root"
    root 'pages#home'
